0f9a3477ef2a2e9f83e692c8307fb3dcc4f32679
Akmal Fauzan Suranta <akmalfauzan.af@gmail.com>
2021-05-05 17:15:15 +0700

    simulasi

--- SimulasiLapas.py
+++ SimulasiLapas.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = ""
-app.config['MYSQL_DB'] = "penjara lapas"
+app.config['MYSQL_DB'] = "contoh2"
 
 mysql = MySQL(app) 
 
@@ -18,7 +18,7 @@ def index() :
                 email = request.form["email"]
 
                 cur = mysql.connection.cursor()
-                cur.execute("INSERT INTO napi (username,email) VALUES (%s,%s)",(username,email))
+                cur.execute("INSERT INTO validasi (username,email) VALUES (%s,%s)",(username,email))
 
                 mysql.connection.commit()
 
