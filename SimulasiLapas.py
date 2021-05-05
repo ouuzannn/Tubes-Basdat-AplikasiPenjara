766121b88864d88d3e67dca96884ea5cb7fe90d1
Akmal Fauzan Suranta <akmalfauzan.af@gmail.com>
2021-05-05 11:53:08 +0700

    Initial commit

---
+++ SimulasiLapas.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template,request
+
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "penjara lapas"
+
+mysql = MySQL(app) 
+
+@app.route('/',methods=['GET','POST'])
+def index() :
+        if request.method == 'POST' :
+                username = request.form['username']
+                email = request.form["email"]
+
+                cur = mysql.connection.cursor()
+                cur.execute("INSERT INTO napi (username,email) VALUES (%s,%s)",(username,email))
+
+                mysql.connection.commit()
+
+                cur.close()
+
+                return "success"
+        return render_template('index.html')
+
+@app.route('/users')
+def users():
+         cur = mysql.connection.cursor()
+         users = cur.execute("SELECT * FROM validasi")
+
+         if users > 0:
+                 userDetails = cur.fetchall()
+
+                 return render_template('user.html',userDetails=userDetails)
+
+
+
+if __name__ == "__main__" :
+    app.run(debug=True)
